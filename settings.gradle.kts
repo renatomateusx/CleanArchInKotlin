6c6b1f21208c4bc6d65c1d1162ac1657e4e9c932
Renato Mateus <renatomateusx@gmail.com>
2020-10-27 17:12:08 -0300

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "CleanArchKotlin"
+include("application")
---
+++ src/test/kotlin/com/renatomateusx/clean/arch/CleanArchKotlin/CleanArchKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.renatomateusx.clean.arch.CleanArchKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
